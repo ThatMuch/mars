d52ca18fa2a651a49cb44708d8e30f050b09a50b
Mathilde Arconte <signup@marconte.fr>
2020-08-03 14:24:48 +0200

    team section

--- templates/section-team.php
+++ templates/section-team.php
@@ -33,97 +33,92 @@
             </div>
         </div>
         <?php
-        $args = array(
-            'post_type' => 'team',
-            'order' => "ASC"
-        );
-        $the_query = new WP_Query($args);
-        if ($the_query->have_posts()) : ?>
-            <div class="">
-                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                    <div class="team-column">
-                        <div class="section-team__member">
-                            <!-- Image -->
-                            <?php if (get_the_post_thumbnail()) : ?>
-                                <div class="section-team__member__img">
-                                    <img  src="<?php the_post_thumbnail_url('large') ?>" alt="<?php the_title() ?>">
-                                </div>
-                            <?php endif; ?>
-                            <!-- Image -->
-                            <div class="section-team__member__content">
-                                <!-- Name -->
-                                <h4 class="section-team__member__content__name"><?php the_title() ?></h4>
-                                <!-- Name -->
-                                <!-- Job -->
-                                <?php if (get_field('job')) : ?>
-                                    <h5 class="section-team__member__content__job"> <?php echo  get_field('job'); ?></h5>
-                                <?php endif; ?>
-                                <!-- Job -->
-                                <!-- Description -->
-                                <?php if (get_field('description')) : ?>
-                                    <p class="section-team__member__content__desc"> <?php echo  get_field('description'); ?></p>
-                                <?php endif; ?>
-                                <!-- Description -->
-                                <!-- Social medias -->
-                            <?php if (have_rows('links')) : ?>
-                                <?php while (have_rows('links')) : the_row(); ?>
-                                    <ul class="section-team__member__rs">
-                                        <!-- Site -->
-                                        <?php if (get_sub_field('website')) : ?>
-                                            <li>
-                                                <a href="<?php the_sub_field('website'); ?>">
-                                                    <i class="fas fa-globe" aria-hidden="true"></i>
-                                                </a>
-                                            </li>
-                                        <?php endif; ?>
-                                        <!-- Site -->
-                                        <!-- Facebook -->
-                                        <?php if (get_sub_field('facebook')) : ?>
-                                            <li>
-                                                <a href="<?php the_sub_field('facebook'); ?>">
-                                                    <i class="fab fa-facebook" aria-hidden="true"></i>
-                                                </a>
-                                            </li>
-                                        <?php endif; ?>
-                                        <!-- Facebook -->
-                                        <!-- Twitter -->
-                                        <?php if (get_sub_field('twitter')) : ?>
-                                            <li>
-                                                <a href="<?php the_sub_field('twitter'); ?>">
-                                                    <i class="fab fa-twitter" aria-hidden="true"></i>
-                                                </a>
-                                            </li>
-                                        <?php endif; ?>
-                                        <!-- Twitter -->
-                                        <!-- Linkedin -->
-                                        <?php if (get_sub_field('linkedin')) : ?>
-                                            <li>
-                                                <a href="<?php the_sub_field('linkedin'); ?>">
-                                                    <i class="fab fa-linkedin" aria-hidden="true"></i>
-                                                </a>
-                                            </li>
-                                        <?php endif; ?>
-                                        <!-- Linkedin -->
-                                        <!-- Instagram -->
-                                        <?php if (get_sub_field('instagram')) : ?>
-                                            <li>
-                                                <a href="<?php the_sub_field('instagram'); ?>">
-                                                    <i class="fab fa-instagram" aria-hidden="true"></i>
-                                                </a>
-                                            </li>
-                                        <?php endif; ?>
-                                        <!-- Instagram -->
-                                    </ul>
-                                <?php endwhile; ?>
-                            <?php endif; ?>
-                            <!-- Social media -->
-                            </div>
-
-                        </div>
-                    </div>
-                <?php endwhile; ?>
-            </div>
-        <?php endif;
-        wp_reset_query(); ?>
+              $args = array(
+                  'post_type' => 'team'
+                  );
+              $the_query = new WP_Query($args);
+              if ($the_query->have_posts() ): ?>
+                  <div class="row justify-content-center">
+                      <?php  while ( $the_query->have_posts() ): $the_query->the_post(); ?>
+                          <div class="col-sm-5">
+                              <div class="section-team__member">
+                                <!-- Image -->
+                                <?php if (get_the_post_thumbnail()) : ?>
+                                    <img class="section-team__member__img" src="<?php the_post_thumbnail_url('thumbnail')?>" alt="<?php the_title()?>">
+                                <? else : ?>
+                                    <div class="section-team__member__img"></div>
+                                <? endif;?>
+                                <!-- Image -->
+                                  <!-- Name -->
+                                      <h4 class="section-team__member__name"><?php the_title()?></h4>
+                                  <!-- Name -->
+                                  <!-- Job -->
+                                  <?php if (get_field('job') ) : ?>
+                                      <h5 class="section-team__member__job"> <?php echo get_field('job'); ?></h5>
+                                  <?php endif; ?>
+                                  <!-- Job -->
+                                  <!-- Description -->
+                                  <?php if (get_field('description') ) : ?>
+                                      <p class="section-team__member__desc"> <?php echo get_field('description'); ?></p>
+                                  <?php endif; ?>
+                                  <!-- Description -->
+                                  <!-- Social medias -->
+                                  <?php if (have_rows('links')) : ?>
+                                      <?php while ( have_rows('links') ) : the_row(); ?>
+                                          <ul class="section-team__member__rs">
+                                              <!-- Facebook -->
+                                              <?php if (get_sub_field('facebook') ) : ?>
+                                              <li>
+                                                  <a href="<?php the_sub_field('facebook');?>">
+                                                      <i class="fab fa-facebook" aria-hidden="true"></i>
+                                                  </a>
+                                              </li>
+                                              <?php endif; ?>
+                                              <!-- Facebook -->
+                                              <!-- Twitter -->
+                                              <?php if (get_sub_field('twitter') ) : ?>
+                                              <li>
+                                                  <a href="<?php the_sub_field('twitter');?>">
+                                                      <i class="fab fa-twitter" aria-hidden="true"></i>
+                                                  </a>
+                                              </li>
+                                              <?php endif; ?>
+                                              <!-- Twitter -->
+                                              <!-- Linkedin -->
+                                              <?php if (get_sub_field('linkedin') ) : ?>
+                                              <li>
+                                                  <a href="<?php the_sub_field('linkedin');?>">
+                                                      <i class="fab fa-linkedin" aria-hidden="true"></i>
+                                                  </a>
+                                              </li>
+                                              <?php endif; ?>
+                                              <!-- Linkedin -->
+                                              <!-- Instagram -->
+                                              <?php if (get_sub_field('instagram') ) : ?>
+                                              <li>
+                                                  <a href="<?php the_sub_field('instagram');?>">
+                                                      <i class="fab fa-instagram" aria-hidden="true"></i>
+                                                  </a>
+                                              </li>
+                                              <?php endif; ?>
+                                              <!-- Instagram -->
+                                              <!-- Google + -->
+                                              <?php if (get_sub_field('google') ) : ?>
+                                              <li>
+                                                  <a href="<?php the_sub_field('google');?>">
+                                                      <i class="fab fa-google-plus-g" aria-hidden="true"></i>
+                                                  </a>
+                                              </li>
+                                              <?php endif; ?>
+                                              <!-- Google + -->
+                                          </ul>
+                                      <?php endwhile; ?>
+                                  <?php endif; ?>
+                                  <!-- Social media -->
+                              </div>
+                          </div>
+                      <? endwhile; ?>
+                  </div>
+                  <? endif; wp_reset_query(); ?>
     </div>
 </section>
