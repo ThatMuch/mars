2556cf4226b62e9829510a147bf756d2ccf6c979
Mathilde Arconte <signup@marconte.fr>
2021-01-16 21:27:00 +0100

    blogPage section categories

--- templates/wp-home.php
+++ templates/wp-home.php
@@ -10,9 +10,9 @@
 ?>
 
 <?php get_header(); ?>
-<main id="blog">
-<div class="container">
-<section>
+<main id="blog" class="blog">
+
+<section class="container blog__featured">
   <div class="row card-blog-featured">
         <?php $args = array(
             'post_type' => 'post',
@@ -78,16 +78,50 @@
           </div>
 
 </section>
-
-<section></section>
-<section class="catgories"></section>
-<?php the_posts_pagination( array(
+<section class="container blog__list">
+  <div class="d-flex justify-content-center w-100">
+    <button class="btn btn-primary mb-3">Afficher plus</button>
+  </div>
+  <?php the_posts_pagination( array(
 	'mid_size'  => 2,
 	'prev_text' => __( '<', 'stanlee' ),
   'next_text' => __( '>', 'stanlee' ),
   'screen_reader_text' => __( '&nbsp;' )
 ) ); ?>
+</section>
+<section class="blog__categories">
+  <div class="container h-100">
+    <div class="row h-100">
+      <div class="col-sm-3 d-flex align-items-center">
+        <p class="blog__categories__title">Retrouvez nos articles par catégories</p>
+      </div>
+      <div class="col-sm-9 d-flex justify-content-center">
+        <div class="blog__categories__list">
+          <?php $categories = get_categories(array('parent' => $parent_category, 'hide_empty' => 0));?>
+          <?php foreach ($categories as $category) :
+            if ($category->term_id === 1) {continue;}
+            $category_link = get_category_link( $category->term_id );
+            ?>
 
-</div>
+            <a href="<?= esc_url( $category_link );?>" class="blog__categories__list__name">
+              <?php
+                $image = get_field('image', $category);?>
+                <div class="card-blog mb-3">
+                   <div class="card-blog_wrapper ">
+                  <img class="card-blog_img img-shadow" src="<?= $image['url'];?>" alt="<?= $category->cat_name;?>">
+                </div>
+                </div>
+              <p class="blog__categories__list__name">
+                <?= $category->cat_name;?>
+              </p>
+            </a>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 </main>
 <?php get_footer(); ?>
