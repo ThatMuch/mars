6e8994b65ce947e49df2bb51d588c72624d62834
Mathilde Arconte <signup@marconte.fr>
2021-01-23 12:22:40 +0100

    footer add mars settings with subpages

--- functions/functions-custom.php
+++ functions/functions-custom.php
@@ -31,7 +31,41 @@ add_theme_support(
 // Page d'options
 if(function_exists('acf_add_options_page') ) {
     acf_add_options_page();
+add_action('acf/init', 'my_acf_op_init');
+function my_acf_op_init() {
+
+    // Check function exists.
+    if( function_exists('acf_add_options_sub_page') ) {
+
+        // Add parent.
+        $parent = acf_add_options_page(array(
+            'page_title'  => __('Mars Settings'),
+            'menu_title'  => __('Mars Settings'),
+            'redirect'    => false,
+        ));
+
+        // Add sub page.
+        $child = acf_add_options_sub_page(array(
+            'page_title'  => __('Coordonées Settings'),
+            'menu_title'  => __('Coordonées'),
+            'parent_slug' => $parent['menu_slug'],
+        ));
+        // Add sub page.
+        $child = acf_add_options_sub_page(array(
+            'page_title'  => __('Footer Settings'),
+            'menu_title'  => __('Footer'),
+            'parent_slug' => $parent['menu_slug'],
+        ));
+        // Add sub page.
+        $child = acf_add_options_sub_page(array(
+            'page_title'  => __('Support'),
+            'menu_title'  => __('Support'),
+            'parent_slug' => $parent['menu_slug'],
+        ));
+    }
 }
+}
+
 
 /**
  * Change Video Background "Tap to unmute" text
